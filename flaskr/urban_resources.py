e96c165ea614045ee44eb8fc1e3b1c81ddea4808
hermana <aeherman@ualberta.ca>
2022-09-02 23:29:27 -0600

    a tiny amount of static data

---
+++ flaskr/urban_resources.py
@@ -0,0 +1,7 @@
+URBAN_RESOURCE_DATA = {
+    "All Nations Hope Network": {
+        "location": "2735 5th Ave, S4T 0L2, Regina",
+        "phone": "306-924-8424",
+        "hours": "24/7"
+    }    
+}
