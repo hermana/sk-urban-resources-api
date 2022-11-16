ce275ee8d18cf01ec12f55d050dc84779b086ed9
hermana <aeherman@ualberta.ca>
2022-11-15 23:30:44 -0600

    add another test point

--- flaskr/urban_resources.py
+++ flaskr/urban_resources.py
@@ -2,6 +2,13 @@ URBAN_RESOURCE_DATA = {
     "All Nations Hope Network": {
         "location": "2735 5th Ave, S4T 0L2, Regina",
         "phone": "306-924-8424",
-        "hours": "24/7"
-    }    
+        "hours": "24/7",
+        "available": ["food", "water", "coffee"]
+    },
+    "Carmichael Outreach": {
+        "location": "1510-12th Ave",
+        "phone": "306-757-2235", 
+        "hours": "Monday to Friday 9pm - 4pm",
+        "available": ["water", "coffee", "snacks"]
+    }
 }
